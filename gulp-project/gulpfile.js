aab701393a7d9ae304d2b58a0fb117b009bce1b0
TrollHammaren <kell.trollhammaren@hotmail.com>
2016-06-17 12:13:21 -0300

    package sass testing

--- gulp-project/gulpfile.js
+++ gulp-project/gulpfile.js
@@ -1,3 +1,22 @@
 /*jslint node: true*/
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+// Sass compiler
+gulp.task('sass', function () {
+	"use strict";
+
+	return gulp.src('app/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css/'));
+});
+
+// Gulp watch sass
+gulp.task('watch', function () {
+    "use strict";
+
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
