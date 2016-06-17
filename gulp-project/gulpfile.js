a95db564ad1deadcdc068724aa60f962ce12dfa1
TrollHammaren <kell.trollhammaren@hotmail.com>
2016-06-17 13:26:33 -0300

    minify, concate

--- gulp-project/gulpfile.js
+++ gulp-project/gulpfile.js
@@ -1,8 +1,13 @@
 /*jslint node: true*/
 
+/* Imports packages
+============================================*/
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
 
 // Sass compiler
 gulp.task('sass', function () {
@@ -20,3 +25,20 @@ gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', ['sass']);
 });
 
+// Concatenete and minify js and css files 
+gulp.task('minify', function () {
+	"use strict";
+
+	return gulp.src('app/*.html')
+		.pipe(useref())
+		.pipe(gulpIf('*.js', uglify()))
+		.pipe(gulpIf('*.css', cssnano()))
+		.pipe(gulp.dest('dist'));
+});
+
+// Default task
+gulp.task('build', ['sass', 'minify'], function () {
+	"use strict";
+
+	console.log("Building files");
+});
