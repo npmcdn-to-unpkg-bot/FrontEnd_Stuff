7a36410fa21d129b2c1253b457d27840d4bb39f3
TrollHammaren <kell.trollhammaren@hotmail.com>
2016-07-12 07:15:49 -0300

    show button works

--- eloquent-javascript/closures-function-in-action/js/main.js
+++ eloquent-javascript/closures-function-in-action/js/main.js
@@ -1,40 +1,52 @@
 "use strict";
 
+// Class person
+var Person = function(firstAndLast) {
+    // Constructor
+    this.firstAndLast = firstAndLast;
+};
+
+// Closures function : Prototype
+(function() {
+    this.getFirstName = function() {
+      return this.firstAndLast.split(" ")[0];
+    };
+    this.getLastName = function() {
+      return this.firstAndLast.split(" ")[1];
+    };
+    this.getFullName = function() {
+      return this.firstAndLast;
+    };
+    this.setFirstName = function(first) {
+      this.firstAndLast.split(" ")[0] = first;
+    };
+    this.setLastName = function(last) {
+      this.firstAndLast.split(" ")[1] = last;
+    };
+    this.setFullName = function(fullName) {
+      this.firstAndLast = fullName;
+    };
+}).call(Person.prototype);
+
 // Buttons
-var showButton = document.querySelector(".show-button");
-var changeButton = document.querySelector(".change-button");
+var showButton = document.querySelector("#show-button");
+var changeButton = document.querySelector("#change-button");
 
-showButton.addEventListener("click", function() {
-  
-  // text content
-  var firstName = document.getElementById("first-name").textContent;
-  var lastName = document.getElementById("last-name").textContent;
-  var fullName = document.getElementById("full-name").textContent;
-  
-  // Class person
-  var Person = function(firstAndLast) {
-      // Constructor
-      this.firstAndLast = firstAndLast;
-  };
+// Fields 
+var firstNameField = document.querySelector("#first-name-field").value;
+var lastNameField = document.querySelector("#last-name-field").value;
+var fullNameField = document.querySelector("#full-name-field").value;
 
-  // Closures function : Prototype
-  (function() {
-      this.getFirstName = function() {
-          return this.firstAndLast.split(" ")[0];
-      };
-      this.getLastName = function() {
-        return this.firstAndLast.split(" ")[1];
-      };
-      this.getFullName = function() {
-        return this.firstAndLast;
-      };
-  }).call(Person.prototype);
- 
+showButton.addEventListener("click", function(event) {
+  
+  event.preventDefault();
+
   // Object Person
-  var bob = new Person("Bob Ross");
-  
-  document.getElementById("first-name").innerHTML = firstName + bob.getFirstName();
-  document.getElementById("last-name").innerHTML = lastName + bob.getLastName();
-  document.getElementById("full-name").innerHTML = fullName + bob.getFullName();
+  var bob = new Person(fullNameField);
+  bob.setFullName(fullNameField);
+
+  document.getElementById("first-name").innerHTML = bob.getFirstName();
+  document.getElementById("last-name").innerHTML = bob.getLastName();
+  document.getElementById("full-name").innerHTML = bob.getFullName();
 
 });
