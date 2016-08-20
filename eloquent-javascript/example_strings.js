b791a23e5a8a61a69ac8963a862d75bd9592237f
TrollHammaren <kell.trollhammaren@hotmail.com>
2016-08-20 18:09:35 -0300

    Refatoring example_strings

--- eloquent-javascript/example_strings.js
+++ eloquent-javascript/example_strings.js
@@ -1,10 +1,13 @@
 'use strict';
 (function() {
-    
+         
     // Pick value direct from terminal
     var numberCows = process.argv[2];
     var numberChickens = process.argv[3];
+    var numberPigs = process.argv[4];
 
+    // Refatoring
+    /*
     function farm(cows, chickens) {
         var strCows = String(cows);
         var strChickens = String(chickens);
@@ -17,10 +20,24 @@
             strChickens = "0" + strChickens;
         console.log(strChickens);
     }
+    */
 
-    if(numberChickens == undefined || numberCows == undefined) {
+    function zeroPad(number, width) {
+        var strNumber = String(number);
+        while(strNumber.length < width)
+            strNumber = "0" + strNumber;
+        return strNumber;
+    };
+
+    function farmInventory(cows, chickens, pigs) {
+        console.log(zeroPad(cows, 3) + " Cows");
+        console.log(zeroPad(chickens, 4) + " Chickens")
+        console.log(zeroPad(pigs, 5) + " Pigs")
+    };
+
+    if(numberChickens == undefined || numberCows == undefined || numberPigs == undefined) {
         console.log("Don't forgot to pass a value direct form terminal");
     } else {
-        farm(numberCows, numberChickens);
+        farmInventory(numberCows, numberChickens, numberPigs);
     }
 })();
